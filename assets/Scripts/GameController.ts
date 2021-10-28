576e4fc8ce4a00d15a459404753b008afd2b303c
Oat-Nattakit <63033946+Oat-Nattakit@users.noreply.github.com>
2021-10-28 17:06:22 +0700

    [Update] Rename Function

--- assets/Scripts/GameController.ts
+++ assets/Scripts/GameController.ts
@@ -29,23 +29,27 @@ export default class NewClass extends cc.Component {
     @property(cc.Integer)
     private StartBalance: number = 10000;
 
-    private LineBetValue : number = 0;
-    private Bet_Price : number = 0;
+    private LineBetValue: number = 0;
+    private Bet_Price: number = 0;
 
     start() {
 
         this.Payline = Payline_Manager.GetIns_();
         this.Bet = Bet_System.GetIns();
-        this.UI_Manager.Spin_Button.node.on('click', this.Spin_Slot, this);
-        this.UI_Manager.Add_Bet.node.on('click', this.Bet_Manager_Add, this);
-        this.UI_Manager.Del_Bet.node.on('click', this.Bet_Manager_Del, this)
+        this.SetButton_Function();
         this.UI_Manager.ShowCurrentBalance(this.StartBalance.toString());
-        this.UI_Manager.Add_Line.node.on('click', this.LineBet_Add, this);
-        this.UI_Manager.Del_Line.node.on('click', this.LineBet_Del, this);
         this.LineBetValue = this.Bet.LineBet_Start(this.Payline.PaylineList.length);
         this.Bet_Price = this.Bet.Bet_Control(0);
-        this.UpdateDataBut();
-        
+        this.UpdateData_TotalBet();
+
+    }
+
+    private SetButton_Function() {
+        this.UI_Manager.Spin_Button.node.on('click', this.Spin_Slot, this);
+        this.UI_Manager.Add_Bet.node.on('click', this.Bet_Manager_Add, this);
+        this.UI_Manager.Del_Bet.node.on('click', this.Bet_Manager_Del, this);
+        this.UI_Manager.Add_Line.node.on('click', this.LineBet_Add, this);
+        this.UI_Manager.Del_Line.node.on('click', this.LineBet_Del, this);
     }
 
     private Spin_Slot() {
@@ -70,36 +74,36 @@ export default class NewClass extends cc.Component {
         if (GetPlayerPayline.length != 0) {
 
             this.UI_Manager.PlayerGetBouns();
-            this.Cal_Bonus();            
+            this.Cal_Bonus();
         }
     }
 
     private Bet_Manager_Add() {
-        this.Bet_Price = this.Bet.Bet_Control(1);        
-        this.UpdateDataBut();
+        this.Bet_Price = this.Bet.Bet_Control(1);
+        this.UpdateData_TotalBet();
     }
 
     private Bet_Manager_Del() {
-        this.Bet_Price = this.Bet.Bet_Control(-1);        
-        this.UpdateDataBut();
+        this.Bet_Price = this.Bet.Bet_Control(-1);
+        this.UpdateData_TotalBet();
     }
 
-    private LineBet_Add() {        
-        this.LineBetValue = this.Bet.Line_Control(1);      
-        this.UpdateDataBut();
+    private LineBet_Add() {
+        this.LineBetValue = this.Bet.Line_Control(1);
+        this.UpdateData_TotalBet();
     }
     private LineBet_Del() {
         this.LineBetValue = this.Bet.Line_Control(-1);
-        this.UpdateDataBut();
+        this.UpdateData_TotalBet();
     }
-    private UpdateDataBut(){
+    private UpdateData_TotalBet() {
         this.UI_Manager.ShowCurrentBet(this.Bet_Price);
         this.UI_Manager.ShowCurrentLineBet(this.LineBetValue);
         let TotalBet_Value = (this.LineBetValue * this.Bet_Price);
         this.UI_Manager.TotalBet_Show(TotalBet_Value);
     }
 
-    private Cal_Bonus(){
+    private Cal_Bonus() {
         let PlayBonuse_Value = (this.LineBetValue * this.Bet_Price);
         //this.UI_Manager.ShowPriceBonus(PlayBonuse_Value);
     }
